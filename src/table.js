3e2ab6dd94956ce2ade9a7f059b1ada656d56b5b
CFaut <86919186+CFaut@users.noreply.github.com>
2024-01-16 13:17:19 +0200

    Updates to assigment

--- src/table.js
+++ src/table.js
@@ -1,7 +1,11 @@
-function Table() {
+import "./css/table.css"
+
+function Table({length}) {
     
-let currentDate = '2016-05-18';
+let currentDate = '2016-05-20';
 let currentTime = '11:27:00';
+
+console.log(length)
 
 function checkSlotAvailability (time, jobLength, date, availability) {
     // your code here
@@ -59,9 +63,87 @@ function checkSlotAvailability (time, jobLength, date, availability) {
         return "AVAILABLE";
     };
 
+    const data = [
+        {
+        "Date": "2016-05-18",
+        "HoursAvailable": [9, 10, 11, 12, 13, 14, 17]
+        },
+        {
+        "Date": "2016-05-19",
+        "HoursAvailable": [9, 10, 11, 12, 13, 14, 15, 16, 17]
+        },
+        {
+        "Date": "2016-05-20",
+        "HoursAvailable": [9, 10, 14, 15, 16, 17]
+        },
+        {
+        "Date": "2016-05-21",
+        "HoursAvailable": [9, 10, 11, 12, 13]
+        },
+        {
+        "Date": "2016-05-23",
+        "HoursAvailable": [13, 14, 15, 16]
+        },
+        {
+        "Date": "2016-05-24",
+        "HoursAvailable": [11, 12, 15, 16, 17]
+        }
+    ];
+
+
     return (
       <div>
-        <h1>Table</h1>
+        <h1>Step 2. Select an arrival time.</h1>
+        <div className="booking-table">
+            <table>
+                <tr>
+                    <th>time slots</th>  
+                    <th>9-10</th>
+                    <th>10-11</th>
+                    <th>11-12</th>
+                    <th>12-13</th>
+                    <th>13-14</th>
+                    <th>14-15</th>
+                    <th>15-16</th>
+                    <th>16-17</th>
+                    <th>17-18</th>
+                </tr>
+                {data.map((data) => (
+                    <div>
+                    <tr> 
+                        <td>{data.Date}</td>
+                        <td>{checkSlotAvailability(9, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(10, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(11, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(12, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(13, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(14, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(15, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(16, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    <tr>
+                        <td>{checkSlotAvailability(17, length, data.Date, data.HoursAvailable)}</td>
+                    </tr>
+                    </div>
+                    )) }
+                
+            </table>
+
+        </div>
       </div>
     );
   }
